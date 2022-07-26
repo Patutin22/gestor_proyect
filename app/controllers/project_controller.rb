e0781111b93c44a9e6aecb771b0739327cc2933c
Patricia <patriciaespejoq@gmail.com>
2022-07-25 21:41:25 -0400

    commit a github

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,4 +1,16 @@
 class ProjectController < ApplicationController
     def index
+        @projects = Project.all
     end
+    
+    def create
+        project = Project.create(
+          name: params[:name],
+          description: params[:description],
+          start_date: params[:start_date],
+          end_date: params[:end_date],
+          state: params[:state]
+        )
+    end
+    
 end
